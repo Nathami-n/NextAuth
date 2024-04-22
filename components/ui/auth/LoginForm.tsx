bf3733d4b058788f7620a9c11b42b7e2da4c384f
Nathan George <georgenathan010@gmail.com>
2024-04-22 14:15:27 +0300

    Added zod and react-hook-form

--- components/ui/auth/LoginForm.tsx
+++ components/ui/auth/LoginForm.tsx
@@ -1,6 +1,27 @@
-import { CardWrapper } from "./CardWrapper"
+import { CardWrapper } from "./CardWrapper";
+import {useForm} from 'react-hook-form';
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from 'zod';
+import { LoginSchema } from "@/schemas";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormLabel,
+    FormMessage,
+
+} from '@/components/ui/form';
 
 export const LoginForm = () => {
+
+    const form = useForm<z.infer<typeof LoginSchema>>({
+        resolver: zodResolver(LoginSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        }
+    })
+
     return (
        <CardWrapper 
        headerLabel="Welcome Back"
