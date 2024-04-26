6dcc33cd53b35d60e7a94cde98f0756079e97982
Nathan-alba <georgenathan010@planetalba.com>
2024-04-26 09:51:43 +0300

    Added the register server action

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,8 @@
 "use server";
 import * as z from 'zod';
 import { RegisterSchema } from '@/schemas';
+import bcrypt from 'bcrypt';
+import {client} from '@/utils/db';
 
 export const register = async (values:z.infer< typeof RegisterSchema> ) => {
     const validFields = RegisterSchema.safeParse(values);
@@ -11,17 +13,53 @@ export const register = async (values:z.infer< typeof RegisterSchema> ) => {
             error: "Invalid credentials",
             success:{
                 state: false,
-                response: undefined
+                response: undefined,
+                data: null
             }
         }}
     };
+
+    const {
+        email,
+        password,
+        name
+    } = validFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await client.user.findUnique({
+        where: {
+            email
+        }
+    });
+
+    if(existingUser) {
+        return {
+            data: {
+                error: undefined,
+                success:{
+                    state: false,
+                    response: "User already exists",
+                    data: null
+                }
+            }
+        }
+    };
+
+    await client.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword
+        }
+    })
 
     return {
          data: {
         error: undefined,
         success: {
             state: true,
-            response: "Success in logging in"
+            response: "Success in logging in",
+            data: null
         }
     }}
 }
